97c2b3c4ff8a3e4bd34de56327817ece993682da
pguduguntla <praneethg347@gmail.com>
2021-04-09 14:47:38 -0500

    Add latest UI plus real-time status collection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Recorder } from './components/Recorder';
+import { Layout } from 'antd';
+
+
+const { Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="main-container">
+      <Layout style={{ backgroundColor: "black", color: "white" }}>
+        <Content>
+          <Recorder />
+        </Content>
+      </Layout>
+
     </div>
   );
 }
---
+++ src/components/Library.js
@@ -0,0 +1,46 @@
+import { Row, Card} from 'antd';
+
+
+export const Library = ({ recordings }) => {
+    return (
+        <div>
+            <p>My Recordings</p>
+            { recordings.length == 0 ?
+                <EmptyLibraryCard />
+                :
+                recordings.slice(0).reverse().map((cleanRecording, index) => {
+                    return <AudioCard cleanRecording={cleanRecording} count={recordings.length - index} />
+                })
+            }
+        </div>
+    );
+}
+
+const AudioCard = ({ cleanRecording, count }) => {
+    return (
+        <Row
+            gutter={4}
+            style={{ width: "100%" }}
+            justify="center"
+            align="middle">
+            <Card bordered={true} style={{ backgroundColor: "#141414", color: "white", borderColor: "#272727", width: 400, padding: 0, marginBottom: 15, textAlign: "left" }}>
+                <p>{"Recording " + count}</p>
+                <audio style={{ width: "100%" }} src={cleanRecording} controls="controls" />
+            </Card>
+        </Row>
+    )
+}
+
+const EmptyLibraryCard = () => {
+    return (
+        <Row
+            gutter={4}
+            style={{ width: "100%" }}
+            justify="center"
+            align="middle">
+            <Card bordered={true} style={{ backgroundColor: "#141414", color: "white", borderColor: "#272727", width: 400, marginBottom: 15, textAlign: "left" }}>
+                <p style={{ margin: "0" }}>You currently do not have any recordings.</p>
+            </Card>
+        </Row>
+    )
+}
